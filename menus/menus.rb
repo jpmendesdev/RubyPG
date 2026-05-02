f655b71897bcca11a1944fb097a8d59084b3dca1
João Pedro <jotamendes944@gmail.com>
2026-05-02 20:18:24 -0300

    .

--- menus/menus.rb
+++ menus/menus.rb
@@ -9,7 +9,6 @@ def game_menu
     if players.length == 0
       puts "
       1 - Create Player
-      2 - Phases
       3 - Exit
         "
     elsif battles.length == 0
@@ -21,9 +20,10 @@ def game_menu
       puts "3 - Exit"
     end
     option = gets.chomp
-    if option == '1'
+    case
+    when option == '1'
        player_menu(players)
-    elsif option == '2' and battles.length == 0
+    when option == '2'
        battle_menu(battles,players)
     else
       break
@@ -34,25 +34,42 @@ end
 def player_menu(players)
   puts "<<Write the player's name>>"
       player_name = gets.chomp
-      default_player = Player.new(player_name,100,['silver sword'],1)
-      players.push(default_player)
-      puts default_player
+      if player_name.length >= 3
+        default_player = Player.new(player_name,100,['silver sword'],1)
+        players.push(default_player)
+        puts default_player
+      else
+        puts "Your name have to be at least 3 characters!"
+      end
 end
 
 def battle_menu(battles,players)
-  random_enemy_number = rand(1..2)
-  enemy = Enemy.allocate
-  if random_enemy_number == 1
-    enemy = SlimeWorm.new
-  elsif random_enemy_number == 2
-    enemy = StoneWatchMan.new
+  for phase in 1..3
+    puts "
+      1 - GO to phase #{phase}
+      2 - exit
+    "
+    option = gets.chomp
+    case
+    when option == '1'
+      players[0].health_points = 100
+      random_enemy_number = rand(1..2)
+      enemy = Enemy.allocate
+      if random_enemy_number == 1
+        enemy = SlimeWorm.new
+      elsif random_enemy_number == 2
+        enemy = StoneWatchMan.new
+      end
+      puts "
+      <<Battle #{phase}>>
+      #{players[0].name}: ❤️ #{players[0].health_points} X #{enemy.name}: ❤️ #{enemy.health_points}
+      "
+      battles.push(1)
+      battle_round(players[0],enemy)
+    when option == '3'
+      break
+    end
   end
-  puts "
-  <<Battle 1>>
-  #{players[0].name}: #{players[0].health_points} X #{enemy.name}: #{enemy.health_points}
-  "
-  battles.push(1)
-  battle_round(players[0],enemy)
 end
 
 def battle_round(player,enemy)
@@ -64,16 +81,20 @@ def battle_round(player,enemy)
       if round == 1
         puts "You're attacking"
         enemy.get_damage(player_damage)
-        puts "#{player.name}:#{player.health_points} X #{enemy.name} #{enemy.health_points}"
+        puts "#{player.name}: ❤️ #{player.health_points} X #{enemy.name} ❤️ #{enemy.health_points}"
       elsif round == 2
         puts "Enemy attacking"
         player.get_damage(enemy_damage)  
-        puts "#{player.name}:#{player.health_points} X #{enemy.name} #{enemy.health_points}"  
+        puts "#{player.name}: ❤️ #{player.health_points} X #{enemy.name} ❤️ #{enemy.health_points}"  
       end
     else
       puts "Round finished"
       result = enemy.health_points > player.health_points ? "Enemy Wins" : "You win"
       puts result
+      if result == 'You win'
+        player.level += 1
+        puts"LEVEL UP! 🌟 #{player.level}"
+      end
       break
     end
   end
