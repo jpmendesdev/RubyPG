3c1471d3fe1e0693eab2023d72eba463edeadb69
João Pedro <jotamendes944@gmail.com>
2026-05-03 16:22:21 -0300

    Refactorign menus.rb file using advatanges of ruby

--- menus/menus.rb
+++ menus/menus.rb
@@ -5,29 +5,25 @@ def game_menu
   players = []
   enemies = []
   battles = []
-  while true
-    if players.length == 0
-      puts "
-      1 - Create Player
-      3 - Exit
-        "
-    elsif battles.length == 0
-      puts "
-      2 - Phases
-      3 - Exit
-        "
-    else
+  loop do
+      display_options(players,battles)
+      option = gets.chomp
+      case option
+      when '1' then player_menu(players)
+      when '2' then battle_menu(battles,players)
+      when '3' then break
+      else puts "Invalid option!"
+      end
+  end
+end
+
+def display_options(players,battles)
+  if players.empty?
+      puts "1 - Create Player\n3 - Exit"
+  elsif battles.empty?
+      puts "2 - Phases\n3 - Exit"
+  else
       puts "3 - Exit"
-    end
-    option = gets.chomp
-    case
-    when option == '1'
-       player_menu(players)
-    when option == '2'
-       battle_menu(battles,players)
-    else
-      break
-    end
   end
 end
 
@@ -36,7 +32,7 @@ def player_menu(players)
       player_name = gets.chomp
       if player_name.length >= 3
         default_player = Player.new(player_name,100,['silver sword'],1)
-        players.push(default_player)
+        players.push << default_player # push
         puts default_player
       else
         puts "Your name have to be at least 3 characters!"
@@ -44,59 +40,42 @@ def player_menu(players)
 end
 
 def battle_menu(battles,players)
-  for phase in 1..3
-    puts "
-      1 - GO to phase #{phase}
-      2 - exit
-    "
-    option = gets.chomp
-    case
-    when option == '1'
-      players[0].health_points = 100
-      random_enemy_number = rand(1..2)
-      enemy = Enemy.allocate
-      if random_enemy_number == 1
-        enemy = SlimeWorm.new
-      elsif random_enemy_number == 2
-        enemy = StoneWatchMan.new
-      end
-      puts "
-      <<Battle #{phase}>>
-      #{players[0].name}: ❤️ #{players[0].health_points} X #{enemy.name}: ❤️ #{enemy.health_points}
-      "
-      battles.push(1)
-      battle_round(players[0],enemy)
-    when option == '3'
-      break
-    end
+  player = players.first
+  (1..3).each do |phase|
+    puts "\n1 - GO to phase #{phase}\n2 - exit"
+    return if gets.chomp == '2'
+    player.health_points = 100
+    enemy = [SlimeWorm,StoneWatchMan].sample.new
+    puts "\n<<Battle #{phase}>>\n#{player.name}: ❤️ #{player.health_points} X #{enemy.name}: ❤️ #{enemy.health_points}"
+    battles << 1
+    battle_round(player,enemy)
   end
 end
 
 def battle_round(player,enemy)
-  while true
-    enemy_damage = enemy.damage
-    player_damage = player.damage
-    round = rand(1..2)
-    if player.health_points > 1 and enemy.health_points > 1
-      if round == 1
-        puts "You're attacking"
-        enemy.get_damage(player_damage)
-        puts "#{player.name}: ❤️ #{player.health_points} X #{enemy.name} ❤️ #{enemy.health_points}"
-      elsif round == 2
-        puts "Enemy attacking"
-        player.get_damage(enemy_damage)  
-        puts "#{player.name}: ❤️ #{player.health_points} X #{enemy.name} ❤️ #{enemy.health_points}"  
+   loop do
+      unless player.health_points > 0 && enemy.health_points > 0
+        finish_battle(player,enemy)
+        break
       end
+    if [true,false].sample
+      puts "You're attacking"
+      enemy.get_damage(player.damage)
     else
-      puts "Round finished"
-      result = enemy.health_points > player.health_points ? "Enemy Wins" : "You win"
-      puts result
-      if result == 'You win'
-        player.level += 1
-        puts"LEVEL UP! 🌟 #{player.level}"
-      end
-      break
+      puts "Enemy attacking"
+      player.get_damage(enemy.damage)
     end
+    puts "#{player.name}: ❤️ #{player.health_points} X #{enemy.name} ❤️ #{enemy.health_points}"
+  end
+end
+
+def finish_battle(player,enemy)
+  puts "Round finished"
+  if player.health_points > enemy.health_points
+    player.level += 1
+    puts "LEVEL UP🌟: #{player.level}"
+  else
+    puts "Enemey Wins!"
   end
 end
 
